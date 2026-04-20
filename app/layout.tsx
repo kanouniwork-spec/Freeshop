c1334b037804d4b5d7b9ffc5ff4297ba654de7bd
kanouniwork-spec <kanouniwork@gmail.com>
2026-04-20 11:34:19 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
-export const metadata = {
-  title: "DJ FreeShop",
-  description: "Luxury Store",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0 }}>
-        {children}
-      </body>
+    <html>
+      <body>{children}</body>
     </html>
   );
 }
